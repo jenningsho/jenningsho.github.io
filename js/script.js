46f9b16ccb47c895dc71927f89882d3966febfe3
jenningsho <hojennings@hotmail.fr>
2023-05-14 20:25:15 +0200

    portfolio

--- js/script.js
+++ js/script.js
@@ -5,9 +5,3 @@ const navLinks = document.querySelector(".nav-links")
 menuHamburger.addEventListener('click',()=>{
     navLinks.classList.toggle('mobile-menu')
 })
-
-window.scroll({
-    top: '100vh',
-
-    behavior: "smooth",
-  });
