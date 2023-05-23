396b28fc77824f6410763a0ab70028d5374313ac
jennings <hojennings@hotmail.fr>
2023-05-23 16:24:41 +0200

    portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 // Menu hamburger evenement au click
 const menuHamburger = document.querySelector(".menu-hamburger");
 const navLinks = document.querySelector(".nav-links");
@@ -7,8 +8,22 @@ menuHamburger.addEventListener('click',()=>{
 });
 
 
-document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '.splide' );
-    splide.mount();
-  } );
+// Observer sur le footer 
+const observer = new IntersectionObserver(function(entries){
+    // console.log(entries);
+        // tableau contenant des intersectionObserverEntry
+    for(var i = 0; i<entries.length;i++)
+    {
+        if(entries[i].isIntersecting)
+        {
+            entries[i].target.classList.add("visible");
+        }
+    }
+}, {
+    threshold : 0.2 // indique la zone a partir de laquelle l'élément devient "visible";
+});
+
+// observer un élément
+observer.observe(document.querySelector("footer"));
+
 
