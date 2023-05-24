fb5d75951302e4a52a3c523bffba5bdc61da83e6
jennings <hojennings@hotmail.fr>
2023-05-24 16:24:22 +0200

    portfolio

--- js/script.js
+++ js/script.js
@@ -27,3 +27,20 @@ const observer = new IntersectionObserver(function(entries){
 observer.observe(document.querySelector("footer"));
 
 
+// Observer sur la liste des svg dans skills.css
+const observerSvg = new IntersectionObserver(function(entries){
+    // console.log(entries);
+        // tableau contenant des intersectionObserverEntry
+    for(var i = 0; i<entries.length;i++)
+    {
+        if(entries[i].isIntersecting)
+        {
+            entries[i].target.classList.add("visible");
+        }
+    }
+}, {
+    threshold : 0.5 // indique la zone a partir de laquelle l'élément devient "visible";
+});
+
+// observer un élément
+observerSvg.observe(document.querySelector(".skills .svg_list"));
